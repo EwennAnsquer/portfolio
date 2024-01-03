36db954d15e1cccdc3ec8a3ccb0212da27dec15e
EwennAnsquer <ewenn.ansq@gmail.com>
2024-01-03 18:07:07 +0100

    ajout d'une video en fond

--- src/components/herobanner/herobanner.jsx
+++ src/components/herobanner/herobanner.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './herobanner.css';
+import video from "../../assets/video/Topographic_Textures.webm"
 
 const HeroBanner = React.forwardRef((props, ref) => {
   return (
     <div className='herobanner' ref={ref}>
       <div className='hContainer'>
+        <video className='video-background' src={video} autoPlay muted loop/>
         <h1 className='hTitle'>Ewenn Ansquer</h1>
       </div>
     </div>
