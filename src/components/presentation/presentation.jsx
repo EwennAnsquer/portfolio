e03acc11a56ce2f4569c2164a8c34fa0bd8c9d68
EwennAnsquer <azbyansquer@gmail.com>
2024-05-18 17:40:36 +0200

    changement du texte de présentation

--- src/components/presentation/presentation.jsx
+++ src/components/presentation/presentation.jsx
@@ -6,7 +6,7 @@ export default function Presentation() {
     <div className='presentation' id='presentation'>
         <h2>Présentation</h2>
         <div className="presentationContent">
-            <p>Je m'appelle Ewenn Ansquer, je vis à Lannion dans les Côtes-d'Armor. <br/>Je vais sur mes 21 ans et je suis en fin de deuxième année de BTS SIO (Service Informatique aux Organisations) au Lycée Félix Le Dantec à Lannion. <br/>Je compte continuer mes études dans l'informatique en faisant une licence voir peut-être continuer sur cinq ans d'études.</p>
+            <p>Je m'appelle Ewenn Ansquer, je vis à Lannion dans les Côtes-d'Armor. <br/>Je vais sur mes 21 ans et je suis en fin de deuxième année de BTS SIO (Service Informatique aux Organisations) au Lycée Félix Le Dantec à Lannion. <br/>Je compte continuer mes études dans l'informatique dans le but de faire cinq ans d'études.</p>
         </div>
     </div>
   )
