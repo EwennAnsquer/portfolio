735c4a9cd87f47ba645bb13799561dc44bf6bcd6
EwennAnsquer <azbyansquer@gmail.com>
2024-05-19 18:54:19 +0200

    changement d'orthographe

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ function App() {
                 {/* <Skills /> */}
                 <Timeline />
                 <Liste name="Certifications" Id="certif" fontSize="50px" fontWeigth="800">
-                    <Certification name="Ansi SecNumAcademie" date="12/11/22" file={[["Ansi_SecNumAcademie", "https://dochub.com/m/shared-document/eansquer-ledantec/qA8ldYxVEkAZNZ8R6MJrpP/ansi-secnumacademie-pdf?dt=hVVuGk6J6Ai7xBVLac1B"]]}/>
+                    <Certification name="ANSSI SecNumAcademie" date="12/11/22" file={[["Ansi_SecNumAcademie", "https://dochub.com/m/shared-document/eansquer-ledantec/qA8ldYxVEkAZNZ8R6MJrpP/ansi-secnumacademie-pdf?dt=hVVuGk6J6Ai7xBVLac1B"]]}/>
                     <Certification name="CNIL" date="18/09/22 et 27/09/22" file={[["Certificat_CNIL_Module1","https://dochub.com/m/shared-document/eansquer-ledantec/r4D6EkZVZJjlYA9VpQXW7O/certificat-cnil-module1-pdf?dt=QknAzcY4rzwTx2vG1paj"],["Certificat_CNIL_Module2","https://dochub.com/m/shared-document/eansquer-ledantec/jo3xELpR3A8k5YkVJBa7nr/certificat-cnil-module2-pdf?dt=uyN427-XjxExivKG7TPL"],["Certificat_CNIL_Module3","https://dochub.com/m/shared-document/eansquer-ledantec/Xv7zYW5RnlX3rd0V2A9egx/certificat-cnil-module3-pdf?dt=QpTyTaVtPa77iBrh1G5o"]]}/>
                     <Certification name="Pix" date="19/01/23" file={[["pix","https://dochub.com/m/shared-document/eansquer-ledantec/P0B76b3K6d8Yv2Awn2y1Gg/pix-pdf?dt=ZMXH_1KJkRgPRfqLxqKr"]]}/> {/* nom afficher sur le lien puis nom du fichier dans dossier certifications*/}
                 </Liste>
